142760392a01bed6313f7207419d6cb05ee39209
Agustin Malugani <agusmalugani97@gmail.com>
2025-05-03 17:11:37 -0300

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ function Hero() {
       <section className="h-100 flex flex-col justify-center items-center text-center">
         <h2 className="text-lg  mb-2">Hola, mi nombre es</h2>
         <h1 className="text-5xl font-bold mb-4">Agustin Malugani</h1>
-        <p className=" max-w-xl">Soy un desarrollador web apasionado por construir soluciones modernas y eficientes, guiado por los principios SOLID para entregar software robusto y de alta calidad.</p>
+        <p className=" max-w-xl">Soy un desarrollador web apasionado por construir soluciones modernas y eficientes. Me guío por los principios SOLID para entregar software robusto y de alta calidad. Sigo buenas prácticas de desarrollo, como el uso de Convencional Commits para mantener un historial de cambios claro y organizado, y estructuro mis proyectos aplicando el patrón Modelo-Vista-Controlador (MVC), lo que facilita la escalabilidad y el mantenimiento del código.</p>
       </section>
     );
   }
