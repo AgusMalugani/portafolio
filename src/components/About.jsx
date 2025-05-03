24db0ed68e9d349055e84ad8247fc2749682a7bb
Agustin Malugani <agusmalugani97@gmail.com>
2025-05-03 17:01:18 -0300

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,16 @@ function About() {
       <section id="about" className="py-20">
         <div className="max-w-4xl mx-auto text-center">
           <h2 className="text-3xl font-bold mb-6">Sobre mí</h2>
-          <p className="">Completé un curso intensivo de más de un año en Egg Corporation, donde adquirí habilidades clave para el trabajo en equipo y conocimientos en Java, Spring Boot y React. Posteriormente, profundicé en el desarrollo de APIs RESTful y Spring Boot a través de un curso especializado en TodoCode, y actualmente me preparo para adentrarme en el desarrollo de microservicios.
+          <p className="">Mis comienzos en la programacion fueron a fines de 2022, completé un curso intensivo de más de un año en Egg Corporation, donde adquirí habilidades clave para el trabajo en equipo y conocimientos en <strong> Java, Spring Boot y React </strong>.
+             Posteriormente, profundicé en el <strong> desarrollo de APIs RESTful y Spring Boot </strong> a través de un curso especializado en TodoCode, 
+             y actualmente me preparo para adentrarme en el desarrollo de microservicios.
 
-En febrero de 2025, finalicé el curso Full Stack en Henry, lo que me permitió ampliar mi experiencia en JavaScript, TypeScript, Node.js, Express, TypeORM, NestJS y React.</p>
+En febrero de 2025, finalicé el curso Full Stack en Henry, lo que me permitió ampliar mi experiencia en  
+<strong> JavaScript, TypeScript, Node.js, Express, TypeORM, NestJS y React</strong>. Hoy en dia se convirtio en mi stack principal.
+
+Actualmente estoy trabajando como desarrollador backend en una empresa de tecnología, donde he tenido la oportunidad 
+de aplicar mis conocimientos en proyectos reales y seguir aprendiendo en un entorno profesional.
+</p>
         </div>
       </section>
     );
